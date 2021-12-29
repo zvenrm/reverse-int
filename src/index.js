c0648955d2a26891c13d3c07471bbbb182f35f14
zvenrm <zvennnnnn@gmail.com>
2021-12-29 19:46:08 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString().split('').reverse();
+    let newNum = ''
+    for (let i of str){
+      if (isFinite(i)){
+        newNum += i;
+      }
+    }
+    return newNum.startsWith('0') ? +newNum.slice(1,) : +newNum;
 }
